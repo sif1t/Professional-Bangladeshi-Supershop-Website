09a6c7bb57e3d0f36df8f8f6c3d2ac3ad02db834
Arifeen Sifat <arifeen.sifat01@gmail.com>
2025-11-23 20:00:41 +0600

    Create individual API routes for each endpoint - proper Next.js structure

---
+++ pages/api/categories/[...slug].js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connectDB = require('../../../server/config/db');
+const categoriesRoutes = require('../../../server/routes/categories');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
+app.use('/', categoriesRoutes);
+
+let isConnected = false;
+
+export default async function handler(req, res) {
+    if (!isConnected) {
+        await connectDB();
+        isConnected = true;
+    }
+    
+    // Reconstruct URL for Express
+    const slug = req.query.slug || [];
+    req.url = '/' + slug.join('/');
+    
+    return app(req, res);
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+        externalResolver: true,
+    },
+};
