5c63719e7073c57921842f54a512d1af026ee472
Arifeen Sifat <arifeen.sifat01@gmail.com>
2025-12-13 21:53:47 +0600

    Add BD Supershop logo as website favicon

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -8,8 +8,9 @@ export default function Document() {
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
                 <meta name="description" content="Professional Bangladeshi Supershop - Fresh groceries delivered to your doorstep" />
                 <meta name="keywords" content="grocery, supershop, bangladesh, online shopping, delivery" />
-                
+
                 {/* Favicon */}
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
